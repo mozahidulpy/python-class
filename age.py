37c24350cb96eba12f321fe5b69c6ee78d0a5c0d
mozahidulpy <innocentshaikat@gmail.com>
2025-05-25 09:02:07 -0400

    Add files via upload

---
+++ age.py
@@ -0,0 +1,10 @@
+personAge = int(input("Please Enter Your Age"))
+
+if personAge <= 1:
+    print("You are Enfant")
+elif  personAge < 2:
+    print("You are Toodler")
+elif personAge < 13 :
+    print("You are Teenager")
+elif personAge >= 20:
+    print("You are Adult")
